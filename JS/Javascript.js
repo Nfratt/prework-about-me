a47805297ca8ee6c8bec8b976ec68639837f34bd
Nick Frattaroli <Nafratt@gmail.com>
2019-07-19 18:48:51 -0400

    adding module 6 and a favicon to my fanpage

---
+++ JS/Javascript.js
@@ -0,0 +1,9 @@
+document.getElementById("button 1").addEventListener("click",function(){
+    document.getElementById("box").style.height= "250px";})
+    
+Document.getElementById("button 2").addEventListener("click",function(){
+    document.getElementById("box").style.color= "blue";})
+
+Document.getElementById("button 3").addEventListener("click",function(){
+    document.getElementById("box").style
+}
