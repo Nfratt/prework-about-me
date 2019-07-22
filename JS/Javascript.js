604d0ccfd6fea92d7a882d091c55ceaae403bac8
Nick Frattaroli <Nafratt@gmail.com>
2019-07-22 13:45:45 -0400

    incuding JS to make buttons work

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -1,9 +1,27 @@
-document.getElementById("button 1").addEventListener("click",function(){
-    document.getElementById("box").style.height= "250px";})
-    
-Document.getElementById("button 2").addEventListener("click",function(){
-    document.getElementById("box").style.color= "blue";})
+document.getElementById("Button 1").addEventListener("click", function(){
 
-Document.getElementById("button 3").addEventListener("click",function(){
-    document.getElementById("box").style
-}
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("Button 2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "blue";
+
+});
+
+
+document.getElementById("Button 3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity =".50";
+
+});
+
+document.getElementById("button 4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px"
+    document.getElementById("box").style.backgroundColor ="orange";
+    document.getElementById("box").style.opacity ="1.0";
+
+
+});
