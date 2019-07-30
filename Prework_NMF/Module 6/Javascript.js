354e4d767678c5fc9a4aed0cfb3a92c58078d2a3
Nick Frattaroli <Nafratt@gmail.com>
2019-07-30 15:30:41 -0400

    adding prework folders

---
+++ Prework_NMF/Module 6/Javascript.js
@@ -0,0 +1,28 @@
+
+    document.getElementById("Button 1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+    document.getElementById("Button 2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "blue";
+
+});
+
+
+document.getElementById("Button 3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity =".50";
+
+});
+
+document.getElementById("button 4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px"
+    document.getElementById("box").style.backgroundColor ="orange";
+    document.getElementById("box").style.opacity ="1.0";
+
+
+});
